89cf29165c8658f6ff753e93ed689ad52dcada12
Restyu <jaanga@live.com>
2017-01-30 16:46:56 +0100

    Contentproviders de contactos , insercion de datos en sqlite y vista de contactos en TextView

--- app/src/main/java/com/example/javi/birthday/MainActivity.java
+++ app/src/main/java/com/example/javi/birthday/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.javi.birthday;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +23,62 @@ public class MainActivity extends AppCompatActivity {
         // se crea la base de datos
         DataBaseManager manager = new DataBaseManager(this);
 
+        button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.textView);
 
+        button.setOnClickListener(this);
 
     }
 
 
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.button:
+
+            contactos();
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+
+    public void contactos(){
+
+
+        DataBaseManager manager = new DataBaseManager(this);
+
+        String[] projeccion = new String[] { ContactsContract.Data._ID, ContactsContract.Data.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.TYPE };
+
+        String selectionClause = ContactsContract.Data.MIMETYPE + "='" +
+                ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE + "' AND "
+                + ContactsContract.CommonDataKinds.Phone.NUMBER + " IS NOT NULL";
+
+        String sortOrder = ContactsContract.Data.DISPLAY_NAME + " ASC";
+
+        Cursor c = getContentResolver().query(
+                ContactsContract.Data.CONTENT_URI,
+                projeccion,
+                selectionClause,
+                null,
+                sortOrder);
+
+        textView.setText("");
+
+
+        while(c.moveToNext()){
+
+            textView.append("Identificador: " + c.getString(0) + " Nombre: " + c.getString(1) + " Número: " + c.getString(2)+  " Tipo: " + c.getString(3)+"\n");
+
+            // se guarda en la base de datos los registros
+            manager.insertar(c.getInt(0),c.getString(1),c.getString(2));
+        }
+
+        c.close();
+
+    }
 
 }
